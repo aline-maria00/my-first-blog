92105c8b0df8c73c0fb83777032dc4790fd07b05
aline_maria00 <alineemaria16@gmail.com>
2019-05-16 15:36:43 -0300

    Editar e adicionar apenas o administrador

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views          #e todas as views do blog
 urlpatterns = [
     path('', views.post_list, name='post_list'), #atribuindo view, chamada post_list
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #criar uma nova url que aponta para post_detail
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
