ea3d7d32cf60baf9c9327efb81c5838fdd05a86c
aline_maria00 <alineemaria16@gmail.com>
2019-05-16 14:01:29 -0300

    Adicionado view e template para cada publicação

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views          #e todas as views do blog
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #atribuindo view, chamada post_list
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #criar uma nova url que aponta para post_detail
 ]
-
